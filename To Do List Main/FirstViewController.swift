a9c932ee11a898c4b939dc7a83092054aebe7154
Steven yang <steven_yang9190@hotmail.com>
2016-04-20 15:33:31 -0700

    first commit

--- To Do List Main/FirstViewController.swift
+++ To Do List Main/FirstViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var toDoList = [NSDictionary]()
 
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil {
+            
+            toDoList = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [NSDictionary]
+
+        }
+    }
+    
+    @IBAction func trashToDo(sender: AnyObject) {
+            setEditing(editing, animated: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+    override func viewDidAppear(animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        let toDoIndex = toDoList[indexPath.row]
+        cell.textLabel?.text = toDoIndex.objectForKey("title") as? String
+        cell.detailTextLabel?.text = toDoIndex.objectForKey("overview") as? String
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            toDoList.removeAtIndex(indexPath.row)
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        }
+    }
+    
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
     }
 
+    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        let itemToMove = toDoList[sourceIndexPath.row]
+        toDoList.removeAtIndex(sourceIndexPath.row)
+        toDoList.insert(itemToMove, atIndex: destinationIndexPath.row)
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
 
+    }
+    
+    override func setEditing(editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        tableView.reloadData()
+    }
+ 
 }
 
--- To Do List Main/SecondViewController.swift
+++ To Do List Main/SecondViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func submitButton(sender: AnyObject) {
+        let textDictionary = ["title": "\(textField.text!)", "overview": "\(textView.text!)"]
+        toDoList.append(textDictionary)
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
 
